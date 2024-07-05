216706ddf962066145a950b011f69afd1082c4da
Paradoxical Serenity <136575352+ParadoxicalSerenity@users.noreply.github.com>
2024-07-05 08:50:04 +0000

    Begin work on the engine

--- source/main.ts
+++ source/main.ts
@@ -1,3 +1,15 @@
 export function version(): string{
     return '0.0.1'
 }
+
+export interface Task {
+    taskName:string
+}
+
+export default class TypeTasker{
+    private tasks: Task[] = [];
+    constructor(){
+
+    }
+    
+}
