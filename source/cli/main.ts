b2509692283bd2ff908c9f1efa8ac2abdee239d0
Paradoxical Serenity <136575352+ParadoxicalSerenity@users.noreply.github.com>
2024-07-05 15:54:19 +0000

    Update logging

--- source/cli/main.ts
+++ source/cli/main.ts
@@ -1,12 +1,23 @@
-import { program } from "commander";
+import { program, Option } from "@commander-js/extra-typings";
 import { argv } from "process";
+import TypeTasker from "../main";
+import { getLogger } from "../logger";
 
 program
   .name("TypeTasker")
   .description("TypeTasker - Typescript first task runner.")
   .version("0.0.4")
-  .action(() => {
-    console.log("TypeTasker");
+  .addOption(
+    new Option("--logLevel <level>", "set log level")
+      .choices(["verbose", "info", "error"] as const)
+      .default("info")
+  )
+
+  .action((option) => {
+    console.log(option.logLevel);
+    const logger = getLogger(option.logLevel);
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const instance = new TypeTasker(logger);
   });
 
 program.parse(argv);
