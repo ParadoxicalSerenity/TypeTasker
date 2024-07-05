7f45a3fb266d2f435b412c1f477cc736aa99c1d5
Paradoxical Serenity <136575352+ParadoxicalSerenity@users.noreply.github.com>
2024-07-05 12:58:24 +0000

    Improve tests

--- source/task/task.test.ts
+++ source/task/task.test.ts
@@ -6,4 +6,18 @@ describe("Tasks", () => {
   it("should not error when creating a task", () => {
     assert.doesNotThrow(() => new TT_Task("Test", () => {}));
   });
+  it("should be able to run arbitrarily defined code", () => {
+    assert.doesNotThrow(
+      () =>
+        new TT_Task("Test", () => {
+          const x = 5;
+          const y = 5;
+          const z = x + y;
+        })
+    );
+  });
+  it("should have a name", () => {
+    const task = new TT_Task("Test", () => {});
+    assert.strictEqual(task.taskName, "Test");
+  });
 });
