761f33d5dde43f404e2f9a8bd164150fa2cda6fd
Paradoxical Serenity <136575352+ParadoxicalSerenity@users.noreply.github.com>
2024-07-07 14:43:12 +0000

    Allow for the possibility that a task can fail and wait for it to succeed

--- source/execution.ts
+++ source/execution.ts
@@ -1,8 +1,15 @@
 import { spawn } from "child_process";
 export async function executeCommand(command: string, args: string[]) {
-  spawn(command, args, {
-    cwd: process.cwd(),
-    detached: true,
-    stdio: "inherit",
+  return new Promise((resolve, reject) => {
+    const cmd = spawn(command, args, {
+      cwd: process.cwd(),
+      detached: true,
+      stdio: "inherit",
+    });
+
+    cmd.on("close", (code: number) => {
+      if (code === 0) resolve(code);
+      reject(code);
+    });
   });
 }
