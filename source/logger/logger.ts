dfe6aad7da8222f5dce1aae8f09d29b65f71e960
Paradoxical Serenity <136575352+ParadoxicalSerenity@users.noreply.github.com>
2024-07-17 08:36:50 +0000

    Improve logging

--- source/logger/logger.ts
+++ source/logger/logger.ts
@@ -1,12 +1,63 @@
 import winston from "winston";
 
-export function getLogger(logLevel: string) {
-  return winston.createLogger({
-    transports: [
-      new winston.transports.Console({
-        format: winston.format.simple(),
-        level: logLevel,
-      }),
-    ],
-  });
+export interface LoggerContract {
+  error: (message: string) => void;
+  warn: (message: string) => void;
+  info: (message: string) => void;
+  verbose: (message: string) => void;
+  debug: (message: string) => void;
+}
+
+export type LoggerParams = {
+  enabled: boolean;
+  logLevel: LogLevel;
+};
+
+export type LogLevel = "error" | "warn" | "info" | "verbose" | "debug";
+
+export class Logger implements LoggerContract {
+  private enabled: boolean;
+  private logLevel: LogLevel;
+  private levelMap: Record<string, number> = {
+    error: 0 as const,
+    warn: 1 as const,
+    info: 2 as const,
+    verbose: 3 as const,
+    debug: 4 as const,
+  } as const;
+
+  private internalLogger: winston.Logger;
+
+  constructor(params: LoggerParams) {
+    this.enabled = params.enabled;
+    this.logLevel = params.logLevel;
+    this.internalLogger = winston.createLogger({
+      transports: [
+        new winston.transports.Console({
+          format: winston.format.simple(),
+          level: params.logLevel,
+        }),
+      ],
+    });
+  }
+  private shouldLog(logLevel: LogLevel): boolean {
+    if (!this.enabled) return false;
+    if (this.levelMap[logLevel] <= this.levelMap[this.logLevel]) return true;
+    return false;
+  }
+  error(message: string) {
+    if (this.shouldLog("error")) this.internalLogger.error(message);
+  }
+  warn(message: string) {
+    if (this.shouldLog("error")) this.internalLogger.warn(message);
+  }
+  info(message: string) {
+    if (this.shouldLog("error")) this.internalLogger.info(message);
+  }
+  verbose(message: string) {
+    if (this.shouldLog("error")) this.internalLogger.verbose(message);
+  }
+  debug(message: string) {
+    if (this.shouldLog("error")) this.internalLogger.debug(message);
+  }
 }
