b4e71730859b9f0cb9a6a0babff7a6c53525a39b
ParadoxicalSerenity <136575352+ParadoxicalSerenity@users.noreply.github.com>
2024-07-18 21:21:16 +0100

    Strategy refactor

--- source/engine/strategy.ts
+++ source/engine/strategy.ts
@@ -1,24 +1,26 @@
 import { Task } from "./typeTasker";
 
 export type StratagyType = "Parallel" | "Serial";
+export type Node = Task | BaseStrategy;
 
 export class BaseStrategy {
   private stratagyType: StratagyType;
-  private tasks: Task[];
-  constructor(stratagyType: StratagyType, tasks: Task[]) {
+  private children: Node[];
+
+  constructor(stratagyType: StratagyType, children: Node[]) {
     this.stratagyType = stratagyType;
-    this.tasks = tasks;
+    this.children = children;
   }
 }
 
 export class Parallel extends BaseStrategy {
-  constructor(tasks: Task[]) {
-    super("Parallel", tasks);
+  constructor(children: Node[]) {
+    super("Parallel", children);
   }
 }
 
 export class Serial extends BaseStrategy {
-  constructor(tasks: Task[]) {
-    super("Serial", tasks);
+  constructor(children: Node[]) {
+    super("Serial", children);
   }
 }
