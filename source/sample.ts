78ea8ad289cd2af8e7fd364f422d4ae00ccb27c3
ParadoxicalSerenity <136575352+ParadoxicalSerenity@users.noreply.github.com>
2024-07-18 21:21:33 +0100

    New example structure

--- source/sample.ts
+++ source/sample.ts
@@ -1,27 +1,25 @@
+import { Parallel, Serial } from "./engine/strategy";
 import { TypeTasker, TypeTaskerCallback } from "./main";
 
 const typeTasker = new TypeTasker({
   logger: { enabled: true, logLevel: "info" },
 });
 
-const test_one = new TypeTaskerCallback({
-  name: "test_one",
-  callback: () => {},
-});
-const test_two = new TypeTaskerCallback({
-  name: "test_two",
-  dependsOn: [test_one],
-  callback: () => {},
-});
-const test_three = new TypeTaskerCallback({
-  name: "test_three",
-  dependsOn: [test_one],
-  callback: () => {},
-});
-const defaultTask = new TypeTaskerCallback({
-  name: "def",
-  dependsOn: [test_one, test_three, test_two],
-  callback: () => {},
-});
-
-typeTasker.run(defaultTask);
+typeTasker.run(
+  new Serial([
+    new TypeTaskerCallback({
+      name: "test_one",
+      callback: () => {},
+    }),
+    new Parallel([
+      new TypeTaskerCallback({
+        name: "test_two",
+        callback: () => {},
+      }),
+      new TypeTaskerCallback({
+        name: "test_three",
+        callback: () => {},
+      }),
+    ]),
+  ])
+);
